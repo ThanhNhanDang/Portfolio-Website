ef2c95a8c2544a6ea0286aa6ca83b3ee0826428f
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-09-11 13:14:53 +0700

    Updates

---
+++ precache-manifest.3ebe233e733b9693fbb2b2fa65030efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37589c40dd9917f4d7c2",
+    "url": "/Portfolio-Website/static/css/main.0e01d2be.chunk.css"
+  },
+  {
+    "revision": "37589c40dd9917f4d7c2",
+    "url": "/Portfolio-Website/static/js/main.37589c40.chunk.js"
+  },
+  {
+    "revision": "edc9b6f448e316d09424",
+    "url": "/Portfolio-Website/static/js/1.edc9b6f4.chunk.js"
+  },
+  {
+    "revision": "9b6b17faf27c71f0d4ca",
+    "url": "/Portfolio-Website/static/js/runtime~main.9b6b17fa.js"
+  },
+  {
+    "revision": "bccf1f859a4fee493a11e2afb13b7a39",
+    "url": "/Portfolio-Website/index.html"
+  }
+];
